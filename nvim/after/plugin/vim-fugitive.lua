656805275ed6464adf21cc2d414b163db21a37af
Glemen Neo <glemenneo@gmail.com>
2024-11-12 15:13:23 +0800

    remove whitespaces

--- nvim/after/plugin/vim-fugitive.lua
+++ nvim/after/plugin/vim-fugitive.lua
@@ -1,10 +1,8 @@
-vim.keymap.set("n", "<leader>gg", vim.cmd.Git, {desc = "Open git visualiser"})
-vim.keymap.set("n", "<leader>gs", ":G status<CR>", {desc = "Git status"})
-vim.keymap.set("n", "<leader>gd", ":G diff<CR>", {desc = "Git diff"})
-vim.keymap.set("n", "<leader>gp", ":G push<CR>", {desc = "Git push"})
-vim.keymap.set("n", "<leader>gl", ":G pull<CR>", {desc = "Git pull"})
-vim.keymap.set("n", "<leader>gf", ":G fetch<CR>", {desc = "Git fetch"})
-vim.keymap.set("n", "<leader>gb", ":G blame<CR>", {desc = "Git blame"})
-vim.keymap.set("n", "<leader>gc", ":G commit<CR>", {desc = "Git commit"})
-
-
+vim.keymap.set("n", "<leader>gg", vim.cmd.Git, { desc = "Open git visualiser" })
+vim.keymap.set("n", "<leader>gs", ":G status<CR>", { desc = "Git status" })
+vim.keymap.set("n", "<leader>gd", ":G diff<CR>", { desc = "Git diff" })
+vim.keymap.set("n", "<leader>gp", ":G push<CR>", { desc = "Git push" })
+vim.keymap.set("n", "<leader>gl", ":G pull<CR>", { desc = "Git pull" })
+vim.keymap.set("n", "<leader>gf", ":G fetch<CR>", { desc = "Git fetch" })
+vim.keymap.set("n", "<leader>gb", ":G blame<CR>", { desc = "Git blame" })
+vim.keymap.set("n", "<leader>gc", ":G commit<CR>", { desc = "Git commit" })
